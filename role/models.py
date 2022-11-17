97117db548941f1e25e96a3af3622a7529a2165f
Safa ELFADHEL <safaelfadhel@gmail.com>
2022-11-17 19:43:27 +0100

    new-version

--- role/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import Permission
-
-class Role(models.Model):
-    name = models.CharField(max_length=50, blank=True, null=True)
-    permissions=models.ManyToManyField(Permission, related_name="PermissionData")
-
-    def __str__(self):
-        return "%s" % (self.name)
--- role/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Role
-from api_back.serializers import PermissionSerializer
-
-class RoleSerializer(serializers.ModelSerializer):
-    PermissionData = PermissionSerializer(source="permissions", many=True, read_only=True)
-    class Meta:
-        model = Role
-        fields = '__all__'
